0e2065ba0c43f699414c4a5bf7dade794d04a7eb
phlira <phlira@igcorp.com.br>
2021-05-13 16:16:01 -0300

    olá usuario colocado

--- src/Painel.js
+++ src/Painel.js
@@ -7,14 +7,22 @@ function Painel() {
   return (
     
     
+    
      <div id="container">
-        
+    
         <div id="left-content">
             <img id="logo-fit" src={Logo}></img>
             <Box />
         </div>
         
         <div id="right-content">
+          <a id="user">
+            Olá usuario
+            <svg width="1550" height="4" viewBox="0 0 1550 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 1L1549 3.09466" stroke="black" stroke-opacity="0.1"/>
+</svg>
+
+          </a>
             <Card />
             <Card />
             <Card />
